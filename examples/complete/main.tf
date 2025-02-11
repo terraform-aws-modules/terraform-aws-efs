b396981579b8567baf04dcf743173be05e274efb
Jack Green <JackPGreen@Gmail.com>
2025-02-11 20:47:03 +0000

    more

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -64,7 +64,7 @@ module "efs" {
   security_group_vpc_id      = module.vpc.vpc_id
   security_group_rules = {
     vpc = {
-      # relying on the defaults provdied for EFS/NFS (2049/TCP + ingress)
+      # relying on the defaults provided for EFS/NFS (2049/TCP + ingress)
       description = "NFS ingress from VPC private subnets"
       cidr_blocks = module.vpc.private_subnets_cidr_blocks
     }
